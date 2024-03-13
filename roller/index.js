7f8da68e4384a98bf9cfa4a19598f5012e0640d7
jesusonoro <ing.jesusonoro@gmail.com>
2024-03-12 20:55:43 -0500

    mar12 855

--- roller/index.js
+++ roller/index.js
@@ -1,13 +1,14 @@
 // Imports
 import * as THREE from "three";
+import { GLTFLoader } from "GLTFLoader";
 import { MindARThree } from "mindar-image-three";
+import { gsap } from "gsap";
 
 // UI Controls
 var backingTargetFound = false;
 const statusText = document.getElementById("statusText");
 
 // ThreeJS + MindAR
-
 // Scene Init
 
 // Backing
@@ -35,6 +36,44 @@ window.addEventListener("click", () => {
   if (!playingVideo && backingTargetFound) {
     video.load();
     video.play();
+
+    if (models) {
+      const showAndClose = (_model, _start, _end) => {
+        setTimeout(() => {
+          gsap.to(_model.object.scale, {
+            x: 1,
+            duration: 1,
+          });
+          gsap.to(_model.object.scale, {
+            y: 1,
+            duration: 1,
+          });
+          gsap.to(_model.object.scale, {
+            z: 1,
+            duration: 1,
+          });
+        }, _start);
+        setTimeout(() => {
+          gsap.to(_model.object.scale, {
+            x: 0,
+            duration: 1,
+          });
+          gsap.to(_model.object.scale, {
+            y: 0,
+            duration: 1,
+          });
+          gsap.to(_model.object.scale, {
+            z: 0,
+            duration: 1,
+          });
+        }, _end);
+      };
+      showAndClose(models[0], 9000, 14000);
+      showAndClose(models[1], 14000, 19000);
+      showAndClose(models[2], 19000, 26000);
+      showAndClose(models[3], 26000, 32000);
+    }
+
     playingVideo = true;
 
     statusText.classList.add("hidden");
@@ -51,6 +90,105 @@ plane.position.set(0, -0.4, 0);
 backingAnchor.group.add(plane);
 
 // 3D Scene
+let modelsReady = 0;
+const models = [
+  {
+    i: 0,
+    name: "Iprasynt",
+    path: "../models/iprasynt_2.glb",
+    position: new THREE.Vector3(0, -0.25, 0.5),
+    mixer: null,
+    playing: false,
+    object: null,
+    zoomIn: null,
+    zoomOut: null,
+    rotate: null,
+    stopRotation: null,
+  },
+  {
+    i: 1,
+    name: "Sacrusynt",
+    path: "../models/sacrusynt_2.glb",
+    position: new THREE.Vector3(0, -0.25, 0.5),
+    mixer: null,
+    playing: false,
+    object: null,
+    zoomIn: null,
+    zoomOut: null,
+    rotate: null,
+    stopRotation: null,
+  },
+  {
+    i: 2,
+    name: "Eclosynt 250",
+    path: "../models/eclosynt250_2.glb",
+    position: new THREE.Vector3(0, -0.25, 0.5),
+    mixer: null,
+    playing: false,
+    object: null,
+    zoomIn: null,
+    zoomOut: null,
+    rotate: null,
+    stopRotation: null,
+  },
+  {
+    i: 3,
+    name: "Eclosynt-Nas",
+    path: "../models/eclosyntNas_2.glb",
+    position: new THREE.Vector3(0, -0.25, 0.5),
+    mixer: null,
+    playing: false,
+    object: null,
+    zoomIn: null,
+    zoomOut: null,
+    rotate: null,
+    stopRotation: null,
+  },
+];
+// GLTF Init
+const gltfLoader = new GLTFLoader();
+const loadModel = (_model) => {
+  gltfLoader.load(
+    _model.path,
+    (gltf) => {
+      console.log(_model.path);
+      console.log(gltf);
+      const object = gltf.scene;
+      _model.object = object;
+
+      // Animation
+
+      const animations = gltf.animations;
+      if (_model.object && animations && animations.length) {
+        _model.mixer = new THREE.AnimationMixer(object);
+      }
+
+      // Positioning
+      _model.object.position.set(
+        _model.position.x,
+        _model.position.y,
+        _model.position.z
+      );
+      _model.object.scale.set(0, 0, 0);
+
+      backingAnchor.group.add(_model.object);
+
+      // Ready
+      modelsReady += 1;
+    },
+    (xhr) => {
+      // console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+    },
+    (error) => {
+      console.log(error);
+      alert(error);
+    }
+  );
+};
+// Load
+models.forEach((_model) => {
+  loadModel(_model);
+});
 
 // Lights
 const environmentLight = new THREE.AmbientLight("#FFFFFF", 3);
